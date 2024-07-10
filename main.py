9f65c12fe43ef73eb4e36b7c7678658a55f55712
GamerC0der <159653318+GamerC0der@users.noreply.github.com>
2024-07-09 18:56:31 -0700

    Minor Changes, Added Ratelimit, 429 Error Page

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, jsonify, render_template, redirect
 import math
+import emoji
+import random
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 
 app = Flask(__name__)
-
+limiter = Limiter(get_remote_address, app=app)
 extra_functions = {
     "sqrt": lambda x: x**0.5,
     "cos": math.cos,
@@ -52,17 +57,22 @@ def calculate(expression):
     try:
         result = eval(expression, {"__builtins__": None}, extra_functions)
         return result
+    except NameError as e:
+        return f"Error: Name '{str(e).split()[1]}' is not defined"
     except KeyError as e:
-        raise ValueError(f"Invalid function or operator: {str(e)}")
+        return f"Invalid function or operator: {str(e)}"
+    except SyntaxError as e:
+        return f"Syntax Error: {str(e)}"
     except Exception as e:
-        raise ValueError(f"Invalid query: {str(e)}")
+        return f"Error: {str(e)}"
 
 
 @app.route('/')
+@limiter.limit("5/minute")
 def index():
     return redirect('/calculator')
 
-
+@limiter.limit("15/minute")
 @app.route('/calculator')
 def calculator():
     return render_template('index.html')
@@ -70,14 +80,22 @@ def calculator():
 def not_found(e):
   return redirect(404)
 
+@app.errorhandler(429)
+def not_found(e):
+  return redirect(429)
+
 @app.route('/404')
 def custom_404():
     return render_template('404.html'), 404
+@app.route('/429')
+def custom_429():
+    return render_template('429.html'), 429
 @app.route('/query', methods=['GET'])
 def query():
     expression = request.args.get('calc')
     if not expression:
         return jsonify({"error": "No expression provided"}), 400
+
 
     try:
         result = calculate(expression)
